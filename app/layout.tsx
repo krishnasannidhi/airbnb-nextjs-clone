1a433c676c113b320601d5b3cbed887e2659b32a
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-07-22 17:06:50 -0500

    added modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar";
+import Modal from "./components/modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal actionLabel="Submit" isOpen title="Hello World" />
         <Navbar />
         {children}
       </body>
